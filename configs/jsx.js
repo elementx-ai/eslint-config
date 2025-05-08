907360d379d7f2aac9829c966f784be60f1a450e
Carter-Apas <57092449+Carter-Apas@users.noreply.github.com>
2025-05-09 11:15:59 +1200

    feat: Add prettier eslint config

--- configs/jsx.js
+++ configs/jsx.js
@@ -1,4 +1,6 @@
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
 import common from "../lib/common";
 import react from "../lib/react.js";
 
-export default [...common, ...react];
+export default [...common, ...react, eslintConfigPrettier];
