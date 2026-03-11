4992f91d47e7241d8abc4e8e1d84d68251500ea7
Carter <57092449+Carter-Apas@users.noreply.github.com>
2026-03-12 11:27:08 +1300

    feat: Add prettier eslint config (#41)

--- configs/js.js
+++ configs/js.js
@@ -1,5 +1,5 @@
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
 import common from "../lib/common.js";
 
-export default [
-  ...common,
-];
+export default [...common, eslintConfigPrettier];
--- configs/ts.js
+++ configs/ts.js
@@ -1,7 +1,6 @@
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
 import common from "../lib/common.js";
 import typescript from "../lib/typescript.js";
 
-export default [
-  ...common,
-  ...typescript,
-];
+export default [...common, ...typescript, eslintConfigPrettier];
