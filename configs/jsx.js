30f8421670676d282747b0f6301ec9f82da34ae1
Carter <57092449+Carter-Apas@users.noreply.github.com>
2026-03-14 21:29:18 +1300

    fix: remove prettier-overridden style rules (#44)

--- configs/jsx.js
+++ configs/jsx.js
@@ -1,6 +1,6 @@
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
-import common from "../lib/common";
+import common from "../lib/common.js";
 import react from "../lib/react.js";
 
 export default [...common, ...react, eslintConfigPrettier];
