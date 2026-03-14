91f76b31505650a1f2119a0460152772a5606bb5
Daniel Jimenez <devildan.knex@gmail.com>
2026-03-15 08:45:02 +1300

    feat: Revert eslint-stylistic changes (#48)

--- configs/jsx.js
+++ configs/jsx.js
@@ -1,6 +1,4 @@
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-
-import common from "../lib/common.js";
+import common from "../lib/common";
 import react from "../lib/react.js";
 
-export default [...common, ...react, eslintConfigPrettier];
+export default [...common, ...react];
--- configs/ts.js
+++ configs/ts.js
@@ -1,6 +1,7 @@
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-
 import common from "../lib/common.js";
 import typescript from "../lib/typescript.js";
 
-export default [...common, ...typescript, eslintConfigPrettier];
+export default [
+  ...common,
+  ...typescript,
+];
